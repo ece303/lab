0a2d35ecdfff37e93f9b3093cb60ffdb86a1bf43
ece303 <hty23@drexel.edu>
2023-10-16 19:46:31 -0300

    feat: two digit test

--- week2/main_twodigit.cpp
+++ week2/main_twodigit.cpp
@@ -1,79 +1,119 @@
 #include <Arduino.h>
 
-int password;  // store the generated password
-int led0 = 8;  // pin number for the LED
-int tog0;      // status of LED status
+int user;
+int user_arr[2];
+int pass[2];
+
+int error = 0;
+
+const int led0_pin = 8;
+const int led1_pin = 9;
+
+int tog0, tog1, password;
 
 void setup() {
-  
-  Serial.begin(9600); 
+  Serial.begin(9600);
 
-  // Initialize the random seed and generate a password between 0 to 9
   randomSeed(analogRead(0));
-  password = random(10);  
+  password = random(99);
   Serial.print("password: ");
-  Serial.println(password);  
+  Serial.println(password);
+  pass[1] = password % 10; password /= 10;
+  pass[0] = password % 10;
 
-  // Set up the LED pin and turn it off initially
-  pinMode(led0, OUTPUT);
-  digitalWrite(led0, LOW);
+  pinMode(led0_pin, OUTPUT);
+  digitalWrite(led0_pin, LOW);
+  
+  pinMode(led1_pin, OUTPUT);
+  digitalWrite(led1_pin, LOW);
 
-  noInterrupts();  
-
-  // Initialize Timer 1 for 1Hz
+  noInterrupts(); 
+  //Timer 1 init to 1Hz
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1 = 0;
-  OCR1A = 15624;  
-  TCCR1B |= (1 << WGM12);  
+  OCR1A = 15624; 
+  TCCR1B |= (1 << WGM12); 
   TCCR1B |= (1 << CS12) | (0 << CS11) | (1 << CS10); 
   TIMSK1 |= (1 << OCIE1A); 
 
-  interrupts();  
+  //Timer 3 init to 1Hz
+  TCCR3A = 0;
+  TCCR3B = 0;
+  TCNT3 = 0;
+  OCR3A = 15624;
+  TCCR3B |= (1 << WGM32); 
+  TCCR3B |= (1 << CS32) | (0 << CS31) | (1 << CS30);
+  TIMSK3 |= (1 << OCIE3A); 
+
+  interrupts(); 
 }
 
-int attempt = 3;  // number of attempts 
+int attempt = 3;
 
-void loop() {  
-  
-  // continue to check for input while there are remaining attempts and serial data is available
-  while(attempt > 0 && Serial.available()){
-    Serial.print("in the while loop, remaining attempt: ");
-    Serial.println(attempt);   
-    
-    String inputStr = Serial.readStringUntil('\n');  // read user's input from serial
-    int userInput = inputStr.toInt();  
-    
-    Serial.println(userInput);   
-    attempt--;  // decrease the number of attempts
+void loop() {
+    while (attempt > 0 && Serial.available()) {
+        Serial.print("in the while loop, remaining attempt: ");
+        Serial.println(attempt);   
 
-    // if user's input matches the password
-    if (userInput == password) {
-      Serial.println("in the Second if statement");
-      tog0 = 1;  
-      Serial.println("UNLOCKED\n");
-      attempt = 0; 
-      break;
-    }  
-    
-    Serial.print("REMAINING attempt: ");
-    Serial.println(attempt);  
+        String inputStr = Serial.readStringUntil('\n');
+        int userInput = inputStr.toInt();
 
-    // change Timer1 frequency and LED status based on remaining attempts
-    if (attempt == 2) {
-      OCR1A = 3124;
-      tog0 = 0;
+        Serial.println(userInput);
+        attempt-=1;
+
+        for (int j = 1; j >= 0; j--) {
+            user_arr[j] = userInput % 10;
+            userInput /= 10;
+        }        
+
+        if ((user_arr[0] == pass[0]) && (user_arr[1] == pass[1])) {
+            tog0 = 1;
+            tog1 = 1;
+            Serial.print("UNLOCKED\n");
+            attempt = 0;
+            break;
+        }
+
+        Serial.print("REMAINING attempt: ");
+        Serial.println(attempt);
+
+        if (attempt == 0){
+            error = 1;
+            tog0 = 0;
+            tog1 = 0;
+            Serial.print("ERROR: No attempt left\n");
+        }
+        else {
+            if (user_arr[0] == pass[0]) tog0 = 1;
+            if (user_arr[1] == pass[1]) tog1 = 1;
+
+            if (attempt == 2) {
+                if (user_arr[0] != pass[0]) {
+                    OCR1A = 3124;
+                    tog0 = 0;
+                }
+                if (user_arr[1] != pass[1]) {
+                    OCR3A = 3124;
+                    tog1 = 0;
+                }
+            }
+        }
     }
-    if (attempt == 1) {
-      OCR1A = 624;
-      tog0 = 0;
-    }      
-    if (attempt == 0) Serial.println("ERROR: No attempt left");  
-  } 
-} 
+}
 
-// interrupt service routine for Timer1 compare match
 ISR(TIMER1_COMPA_vect) {
-  if (tog0 == 1) digitalWrite(led0, LOW);  // turn off the LED if password is correct
-  else digitalWrite(led0, !digitalRead(led0));  // blink the LED if password is wrong
+  if (tog0 == 1) digitalWrite(led0_pin, LOW);
+  else if (tog0 == 0) {
+    if (error) digitalWrite(led0_pin, HIGH);
+    else digitalWrite(led0_pin, !digitalRead(led0_pin));
+  }
+}
+
+ISR(TIMER3_COMPA_vect) {
+  if (tog1 == 1) digitalWrite(led1_pin, LOW);
+  else if (tog1 == 0) {
+    if (error) digitalWrite(led1_pin, HIGH);
+    else digitalWrite(led1_pin, !digitalRead(led1_pin));
+  }
 }
